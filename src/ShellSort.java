224193c92bd9a4edc385f9fb6caec0fe19c1df7c
ptt <691689053@qq.com>
2019-08-05 22:48:34 +0800

    commit

--- src/ShellSort.java
+++ src/ShellSort.java
@@ -1,2 +1,24 @@
+import java.util.Arrays;
+
 public class ShellSort {
+    private static void ShellSort(int[] arr){
+        for(int grap=arr.length/2;grap>0;grap=grap/2){
+            for(int i=grap;i<arr.length;i++){
+                for(int j=i-grap;j>=0;j=j-grap){
+                    int tmp;
+                    if(arr[j]>arr[j+grap]){
+                        tmp=arr[j];
+                        arr[j]=arr[j+grap];
+                        arr[j+grap]=tmp;
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        int arr[]={2,9,1,2,4,6};
+        ShellSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
 }
